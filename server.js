9be3b36f10eca789b235d4467e00a4d92a544ab6
JWalters <joshwalters34@gmail.com>
2021-05-16 21:58:05 -0400

    fixed issue with css files on excercise and stats pages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongojs = require("mongojs");
 const mongoose = require("mongoose");
+const Workout = require("./models")
 
 const path = require('path');
 const PORT = process.env.PORT || 3000;
@@ -25,16 +26,34 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
-// app.get("/", (req, res) => res.sendFile(path.join(__dirname + "./views/index.html")));
-app.get('/stats', (req, res) => res.sendFile(path.join(__dirname, './views/stats.html')));
-app.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, './views/exercise.html')));
-// app.get("/api/notes", (req, res) => {
-// })
-// app.post('/api/notes', (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/index.html'))
+});
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/stats.html'))
+});
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/exercise.html'))
+});
+
+app.get('/api/workouts', (req, res) => {
+//   db.Workout.find({}, (error, data) => {
+//   if (error) {
+//     res.send(error);
+//   } else {
+//     res.json(data)
+//   }
 // });
-// app.delete('/api/notes/:id', (req, res) => {
-// });
+
+db.Workout.find({})
+.then(dbWorkout => {
+  res.json(dbWorkout);
+})
+.catch(err => {
+  res.json(err);
+});
+});
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
